5dceca0448b3dec6cfa64929bcb89c03a2dd5293
Décio Luiz Gazzoni Filho <decio@decpp.net>
2025-02-12 15:22:30 -0300

    Verification-only SQIsign (NIST round 2 version)
    Co-authored-by: Marius A. Aardal <marius.andre.aardal@gmail.com>
    Co-authored-by: Gora Adj <gora.adj@tii.ae>
    Co-authored-by: Diego F. Aranha <dfaranha@cs.au.dk>
    Co-authored-by: Andrea Basso <sqisign@andreabasso.com>
    Co-authored-by: Isaac Andrés Canales Martínez <icanalesm0500@gmail.com>
    Co-authored-by: Jorge Chávez-Saab <jorgechavezsaab@gmail.com>
    Co-authored-by: Maria Corte-Real Santos <mariascrsantos98@gmail.com>
    Co-authored-by: Luca De Feo <github@defeo.lu>
    Co-authored-by: Max Duparc <max.duparc@epfl.ch>
    Co-authored-by: Jonathan Komada Eriksen <jonathan.eriksen97@gmail.com>
    Co-authored-by: Décio Luiz Gazzoni Filho <decio@decpp.net>
    Co-authored-by: Basil Hess <bhe@zurich.ibm.com>
    Co-authored-by: Antonin Leroux <antonin.leroux@polytechnique.org>
    Co-authored-by: Patrick Longa <plonga@microsoft.com>
    Co-authored-by: Luciano Maino <mainoluciano.96@gmail.com>
    Co-authored-by: Michael Meyer <michael@random-oracles.org>
    Co-authored-by: Hiroshi Onuki <onuki@mist.i.u-tokyo.ac.jp>
    Co-authored-by: Lorenz Panny <lorenz@yx7.cc>
    Co-authored-by: Giacomo Pope <giacomopope@gmail.com>
    Co-authored-by: Krijn Reijnders <reijnderskrijn@gmail.com>
    Co-authored-by: Damien Robert <damien.robert@inria.fr>
    Co-authored-by: Francisco Rodríguez-Henriquez <francisco.rodriguez@tii.ae>
    Co-authored-by: Sina Schaeffler <sschaeffle@student.ethz.ch>
    Co-authored-by: Benjamin Wesolowski <benjamin.wesolowski@ens-lyon.fr>

---
+++ crypto_sign/sqisign_lvl1/ref/rng.h
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef rng_h
+#define rng_h
+
+#include "randombytes.h"
+
+#endif /* rng_h */
---
+++ crypto_sign/sqisign_lvl5/ref/api.h
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef api_h
+#define api_h
+
+#include <stddef.h>
+#include <sqisign_namespace.h>
+
+#define CRYPTO_SECRETKEYBYTES 701
+#define CRYPTO_PUBLICKEYBYTES 129
+#define CRYPTO_BYTES 292
+
+#define CRYPTO_ALGNAME "SQIsign_lvl5"
+
+SQISIGN_API
+int
+crypto_sign_keypair(unsigned char *pk, unsigned char *sk);
+
+SQISIGN_API
+int
+crypto_sign(unsigned char *sm, size_t *smlen,
+            const unsigned char *m, size_t mlen,
+            const unsigned char *sk);
+
+SQISIGN_API
+int
+crypto_sign_open(unsigned char *m, size_t *mlen,
+                 const unsigned char *sm, size_t smlen,
+                 const unsigned char *pk);
+
+#endif /* api_h */
